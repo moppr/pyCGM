7858fc5a9f36664efe9ca4dfe0d5b74b0bf7dea8
moppr <mantony42@gmail.com>
2020-11-19 17:39:21 -0500

    Clean up comments to better explain what is being demonstrated

--- demo/user.py
+++ demo/user.py
@@ -1,39 +1,34 @@
 from demo.pycgm import *
 
-# Most basic example, no customization
+# Demonstrates basic case with no customization
 subject0 = CGM()
 subject0.run(0)
 print("Pelvis angles at each frame\n", subject0.pelvis_angles)
-# print("Hip angles at each frame\n", subject0.hip_angles)
-# print("Knee angles at each frame\n", subject0.knee_angles)
 
 
+# Subclass that changes functionality of pelvis_calc to use an additional marker
 class CGM1(CGM):
     @staticmethod
     def pelvis_calc(frame, mapping, mi):
         return frame[mi[mapping["PELV"]]] + frame[mi[mapping["RANK"]]]
 
 
-# Adds extra marker w/ calculation method but no renaming
+# Demonstrates adding a new marker and using a custom calculation with that marker
 subject1 = CGM1()
-subject1.map("RANK", "RANK")
+subject1.map("RANK")
 subject1.run(1)
 print("Pelvis angles at each frame\n", subject1.pelvis_angles)
-# print("Hip angles at each frame\n", subject1.hip_angles)
-# print("Knee angles at each frame\n", subject1.knee_angles)
 
 
-# Renames all the markers at once by providing its own dictionary
+# Demonstrates renaming all markers at once by providing a dictionary
 subject2 = CGM()
 markers2 = {"PELV": "PELVIS", "RHIP": "RIGHTHIP", "LHIP": "LEFTHIP", "RKNE": "RIGHTKNEE", "LKNE": "LEFTKNEE"}
 subject2.map(dic=markers2)
 subject2.run(2)
 print("Pelvis angles at each frame\n", subject2.pelvis_angles)
-# print("Hip angles at each frame\n", subject2.hip_angles)
-# print("Knee angles at each frame\n", subject2.knee_angles)
 
 
-# Renames some markers providing old and new names one at a time, with an extra marker
+# Demonstrates renaming some markers one at a time and adding a new marker w/ custom calculation
 subject3 = CGM1()
 subject3.map("PELV", "PELVIS")
 subject3.map("RKNE", "RKNEE")
@@ -41,5 +36,3 @@ subject3.map("LKNE", "LKNEE")
 subject3.map("RANK", "RANK")
 subject3.run(3)
 print("Pelvis angles at each frame\n", subject3.pelvis_angles)
-# print("Hip angles at each frame\n", subject3.hip_angles)
-# print("Knee angles at each frame\n", subject3.knee_angles)
